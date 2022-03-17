837a98e58ea7793086939194268fa2359c4b76a5
Cristian Franco <cfrancobedoya@gmail.com>
2022-03-17 18:48:48 -0500

    function StickyHeadTable

--- src/containers/dashboardContainer/dashboardDocuments/index.tsx
+++ src/containers/dashboardContainer/dashboardDocuments/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Box } from "@mui/material";
 import { DashboardLayout } from "../../../components/layouts";
+import StickyHeadTable from "../../../components/tables/StickyHeadTable";
 
 function DashboardDocuments() {
   return (
-    <DashboardLayout page="dashboard-documents-page">Dashboard documents page</DashboardLayout>
+    <DashboardLayout page="dashboard-documents-page">
+      <Box sx={{ p: 8, display: "flex", flexDirection: "row" }}>
+        <StickyHeadTable />
+      </Box>
+    </DashboardLayout>
   );
 }
 
--- src/containers/dashboardContainer/dashboardHome/index.tsx
+++ src/containers/dashboardContainer/dashboardHome/index.tsx
@@ -1,3 +1,12 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 import React, { useCallback, useEffect } from "react";
 
 import { DashboardLayout } from "../../../components/layouts";
@@ -18,7 +27,52 @@ function DashboardHome() {
   }, [handleGetUser]);
 
   return (
-    <DashboardLayout page="dashboard-page">Dashboard home page</DashboardLayout>
+    <DashboardLayout page="dashboard-page">
+      <Box sx={{ p: 8, display: "flex", flexDirection: "row" }}>
+        <Grid container spacing={8}>
+          <Grid item xs={6} md={6}>
+            <Card>
+              <CardActionArea sx={{ display: "flex" }}>
+                <CardMedia
+                  component="img"
+                  image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJvHvEkqwHEUdErsaMf_SeJNRTuj82LiE_iznJRrXaHktMQKVHaLx80uRWXTp6Ll3xRvQ&usqp=CAU"
+                  alt="green iguana"
+                  sx={{ width: 300 }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Cristian Franco
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    En este recuadro tenemos información del usuario.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={6} md={6}>
+            <Card>
+              <CardActionArea sx={{ display: "flex" }}>
+                <CardMedia
+                  component="img"
+                  image="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Firma_Len%C3%ADn_Moreno_Garc%C3%A9s.png/1200px-Firma_Len%C3%ADn_Moreno_Garc%C3%A9s.png"
+                  alt="green iguana"
+                  sx={{ width: 300 }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Firma
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    En este recuadro tenemos la firma del usuario.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </DashboardLayout>
   );
 }
 
