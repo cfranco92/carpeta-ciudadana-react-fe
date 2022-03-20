bef7e6f4fd7add3017fb83ef17e3047e2ac1761c
Cristian Franco <cfrancobedoya@gmail.com>
2022-03-20 15:13:08 -0500

    Add DashboardProfile

--- src/containers/dashboardContainer/index.tsx
+++ src/containers/dashboardContainer/index.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 
 import DashboardDocuments from "./dashboardDocuments";
 import DashboardHome from "./dashboardHome";
+import DashboardProfile from "./dashboardProfile";
 
 const DashboardContainer = () => {
   return (
     <Routes>
       <Route path="/" element={<DashboardHome />} />
       <Route path="documents" element={<DashboardDocuments />} />
+      <Route path="profile" element={<DashboardProfile />} />
     </Routes>
   );
 };
